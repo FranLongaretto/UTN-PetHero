db8a10b66ed38885ba1913bec83a78f83fd71013
Franco <franlongaretto@gmail.com>
2022-10-11 19:08:59 -0300

    Initialize petHero

---
+++ Views/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <strong>LabIV - Parcial <?php echo APELLIDO ?></strong>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT."Home/ShowInvoiceList" ?>">Listar Facturas</a>
+          </li>   
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT."Home/Logout" ?>">Cerrar Sesión</a>
+          </li>        
+     </ul>
+</nav>
