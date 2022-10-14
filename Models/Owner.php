42cba95c3f41c3a4c397e9677051925182f8d2b9
tisanti147 <santid147@gmail.com>
2022-10-14 20:27:51 -0300

    clase Dog no va, keeper y owner extienden de user

--- Models/Owner.php
+++ Models/Owner.php
@@ -1,49 +1,10 @@
 <?php
     namespace Models;
 
-    class Owner{
-        private $id;
-        private $email;
-        private $password;
+    use Models\User as User;
 
-          /**
-         * Get the value of id
-         */ 
-        public function getId()
-        {
-                return $this->id;
-        }
-
-        /**
-         * Set the value of id
-         *
-         * @return  self
-         */ 
-        public function setId($id)
-        {
-                $this->id = $id;
-
-                return $this;
-        }
-
-        public function getEmail(){
-            return $this->email;
-        }
-
-        public function setEmail($email){
-            $this->email = $email;
-            return $this;
-        }
-
-
-        public function getPassword(){
-            return $this->password;
-        }
-
-        public function setPassword($password){
-            $this->password = $password;
-            return $this;
-        }
+    class Owner extends User{
+        
 }
 
 ?>
