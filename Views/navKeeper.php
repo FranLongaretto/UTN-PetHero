a0492131b5b51d25b0a6463e6067e8885d60f94b
German Oyarzo <german_oyarzo@hotmail.com>
2022-10-16 20:49:44 -0300

    navbar, inputs and button to back and cancel

--- Views/navKeeper.php
+++ Views/navKeeper.php
@@ -6,7 +6,7 @@
 <body>
 
 <div class="navbar">
-  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+  <a class="active" href="<?php echo FRONT_ROOT."User/HomeKeeper";?>"><i class="fa fa-fw fa-home"></i> Home</a> 
   <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
   <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
   <a href="<?php echo FRONT_ROOT."User/Logout " ?>"><i class="fa fa-fw fa-user"></i> Log Out</a>
