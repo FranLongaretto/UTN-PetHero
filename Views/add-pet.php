231631784cce152c1f352e03458de6aeac59d866
Franco <franlongaretto@gmail.com>
2022-10-25 01:53:58 -0300

    Reestructuracion + estilos addPet + estilos listPet + estilos listKeeper

--- Views/add-pet.php
+++ Views/add-pet.php
@@ -1,54 +1,33 @@
 <?php
   require_once("validate-session.php");
-  include_once("header.php");
   include_once("navOwner.php"); 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<div class="mainForm fadeInDown">
+  <div class="mainForm__container">
+    <!-- Login Form -->
+    <form action="<?php echo FRONT_ROOT?>Pet/Add" method="POST" enctype="multipart/form-data" class="mainForm__form">
+      <h2 class="mainForm__form--title">Add Pet</h2>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Add Pet</title>
-        <link rel="stylesheet" href="<?php echo CSS_PATH ?>style.css">
-    </head>
+      <input type="text" id="race" class="mainForm__form--input fadeIn second" name="race" placeholder="Race" required>
 
-    <body>
-      <div class="wrapper fadeInDown">
-        <div id="formContent">
-          <!-- Tabs Titles -->
+      <select class="fadeIn third" name="size" id="size" required>
+        <option disabled selected>Seleccionar Size</option>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="big">Big</option>
+      </select>
 
-          <!-- Login Form -->
-          <form action="<?php echo FRONT_ROOT?>Pet/Add" method="POST" enctype="multipart/form-data">
-            <h2 class="formTitle">Add Pet</h2>
-  
-            <input type="text" id="race" class="fadeIn second" name="race" placeholder="Race" required>
+      <input type="file" accept="image/*" id="vaccination" class="inputImgVaccine fadeIn second" name="vaccinationImg" required>
 
-            <select class="fadeIn third" name="size" id="size" required>
-              <option disabled selected>Seleccionar Size</option>
-              <option value="small">Small</option>
-              <option value="medium">Medium</option>
-              <option value="big">Big</option>
-            </select>
+      <input type="text" id="description" class="mainForm__form--input fadeIn second" name="description" placeholder="Description" required>
 
-            <input type="file" accept="image/*" id="vaccination" class="inputImgVaccine" name="vaccinationImg" required>
+      <input type="file" accept="image/*" id="image" class="inputImgPet fadeIn second" name="petImage" required>
 
-            <input type="text" id="description" class="fadeIn third" name="description" placeholder="Description" required>
-
-            <input type="file" accept="image/*" id="image" class="inputImgPet" name="petImage" required>
-
-            <div class="formSend">
-              <input type="submit" class="fadeIn fourth formSend__cancel" value="Add Pet">
-              <a href="<?php echo FRONT_ROOT ?>User/HomeOwner" class="formSend__cancel">Cancel</a>
-            </div>
-          </form>
-        </div>
+      <div class="formSend">
+        <input type="submit" class="mainForm__form--submit fadeIn third" value="Add Pet">
+        <a href="<?php echo FRONT_ROOT ?>User/HomeOwner" class="formSend__cancel">Cancel</a>
       </div>
-    </body>
-</html>
-
-<?php
-  include('footer.php');
-?>
+    </form>
+  </div>
+</div>
--- Views/header.php
+++ Views/header.php
@@ -9,11 +9,10 @@
 
      <!-- CSS -->
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>bootstrap.min.css">
-     <link rel="stylesheet" href="<?php echo CSS_PATH ?>footer.css">
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>estilos.css">
-     <link rel="stylesheet" href="<?php echo CSS_PATH ?>style.css">
+     <link rel="stylesheet" href="<?php echo CSS_PATH ?>footer.css">
      <link rel="stylesheet" href="<?php echo CSS_PATH ?>navbar.css">
-     <link rel="stylesheet" href="<?php echo CSS_PATH ?>styleDateFilter.css">
+     <link rel="stylesheet" href="<?php echo CSS_PATH ?>style.css">
 
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
