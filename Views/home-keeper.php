681d0081ea7a18fb481d7b6b9f0cc5e88316309d
Franco <franlongaretto@gmail.com>
2022-10-27 21:19:40 -0300

    Reestructuracion y estilos

--- Views/home-keeper.php
+++ Views/home-keeper.php
@@ -2,24 +2,22 @@
     include_once('navKeeper.php');
     require_once("validate-session.php"); 
 ?>
-<div class="homeOwner"> 
-  <div class="homeOwner__header">
-    <h1 class="homeOwner__title">PET HERO</h1>
-    <p class="homeOwner__subtitle">welcome keeper!!!</p>
+
+<div class="homeUser"> 
+  <div class="homeUser__header">
+    <h1 class="homeUser__title">PET HERO</h1>
+    <p class="homeUser__subtitle">welcome keeper!!!</p>
   </div>
 
-  <div class="homeOwner__menu">
-    <div class="homeOwner__menu--item">
-      <a class="underlineHover" href="<?php echo FRONT_ROOT?>Keeper/RegistrationKeeper">Add Keeper</a>
+  <div class="homeUser__menu">
+    <div class="homeUser__menu--item">
+      <a href="<?php echo FRONT_ROOT?>Keeper/RegistrationKeeper">Add Keeper</a>
     </div>
   
     <?php if($frontMessage){?>
-      <div class="homeOwner__menu--message">
+      <div class="homeUser__menu--message">
         <p class="check__message"><?php echo $frontMessage?></p>
       </div>
     <?php }?>
   </div>
 </div>
-<?php 
-  include_once('footer.php');
-?> 
