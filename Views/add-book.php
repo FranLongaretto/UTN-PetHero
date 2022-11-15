72c0038bfbc5cb93140652c92adb5c4bac905cbe
German Oyarzo <german_oyarzo@hotmail.com>
2022-11-15 17:40:01 -0300

    email

--- Views/add-book.php
+++ Views/add-book.php
@@ -1,6 +1,7 @@
 <?php
   include_once('navOwner.php'); 
   require_once("validate-session.php");
+
 ?>
 
 <div class="mainForm fadeInDown">
@@ -12,30 +13,27 @@
 
       <div class="form-confirmBook">
         <div class="confirmBook-keeper">
-          <p>Keeper Name: </p>
-          <p class="confirmBook-keeper-item"><?php echo $frontKeeper->getFirstName()." ". $frontKeeper->getLastName() ?></p>
-          <input type="string" name="nameKeeper" id="nameKeeper" value="<?php echo $frontKeeper->getFirstName()." ". $frontKeeper->getLastName() ?>" hidden>
+          <h5 style="color:#39ace7">Keeper Name: </h5>
+          <p id="nameKeeper" name="nameKeeper" class="confirmBook-keeper-item"><?php echo $frontKeeper->getFirstName()." ". $frontKeeper->getLastName() ?></p>
           <input type="number" name="idKeeper" id="idKeeper" value="<?php echo $frontKeeper->getId() ?>" hidden>
           <input type="number" name="idOwner" id="idOwner" value="<?php echo $frontOwnerBook->getId() ?>" hidden>
-          <input type="string" name="emailOwner" id="emailOwner" value="<?php echo $frontOwnerBook->getId() ?>" hidden>
           <input type="number" name="idKeeperBook" id="idKeeperBook" value="<?php echo $frontKeeperBook ?>" hidden>
         </div>
         <?php foreach ($frontBookPets as $key => $value) {?>
         <div class="confirmBook-pets">
-          <p>Pet: </p>
-          <p class="confirmBook-pets-item"><?php echo $value->getType() ?> / <?php echo $value->getRace() ?></p>
-          <input type="string" name="pet" id="pet" value="<?php echo $value->getRace() ?>" hidden>
+          <h5 style="color:#39ace7">Pet: </h5>
+          <p id="pet" name="pet" class="confirmBook-pets-item"><?php echo $value->getType() ?> / <?php echo $value->getRace() ?></p>
         </div>
         <?php } ?>
         <div class="confirmBook-book">
           <div class="confirmBook-book-date">
-            <p>Date range: </p>
+            <h5 style="color:#39ace7">Date range: </h5>
             <p class="confirmBook-book-date-item"><?php echo $frontDateStart . " / " . $frontDateEnd ?></p>
             <input type="date" name="dateStart" id="dateStart" value="<?php echo $frontDateStart ?>" hidden>
             <input type="date" name="dateEnd" id="dateEnd" value="<?php echo $frontDateEnd ?>" hidden>
           </div>
           <div class="confirmBook-book-price">
-            <p>Price: </p>
+            <h5 style="color:#39ace7">Price: </h5>
             <p class="confirmBook-book-price-item"><?php echo $frontPrice ?></p>
             <input type="number" name="bookPrice" id="bookPrice" value="<?php echo $frontPrice ?>" hidden>
           </div>
@@ -53,13 +51,14 @@
     </form>
   </div>
 </div>
+
 <script>
-  bookPrice = "Total Price: $"+document.getElementById('bookPrice').value
-  nameKeeper = "Keeper Name:"+document.getElementById('nameKeeper').value
-  idOwner = "Owner Id:"+document.getElementById('idOwner').value
-  pet= "Pet: "+document.getElementById('pet').value
-  dateStart= "Date Start: "+document.getElementById('dateStart').value
-  dateEnd= "Date End: "+document.getElementById('dateEnd').value
+  bookPrice = "Total Price: $".bold()+document.getElementById('bookPrice').value
+  nameKeeper = "Keeper Name: ".bold()+document.getElementById('nameKeeper').innerHTML
+  idOwner = "Owner Id:".bold()+document.getElementById('idOwner').value
+  pet= "Pet: ".bold()+document.getElementById('pet').innerHTML
+  dateStart= "Date Start: ".bold()+document.getElementById('dateStart').value
+  dateEnd= "Date End: ".bold()+document.getElementById('dateEnd').value
   body= bookPrice+"," +nameKeeper+"," + idOwner+"," +pet+"," +dateStart+"," +dateEnd
   console.log(body)
   function sendEmail(){
